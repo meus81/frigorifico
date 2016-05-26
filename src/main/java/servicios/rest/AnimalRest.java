173caab8f7d48c24c665454c2906f6c730c6ecef
danitli <dnannis@gmail.com>
2016-05-26 19:43:42 -0300

    salvando animal desde la ui desktop

--- src/main/java/servicios/rest/AnimalRest.java
+++ src/main/java/servicios/rest/AnimalRest.java
@@ -2,18 +2,22 @@ package servicios.rest;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import bean.tropa.AnimalBean;
 import ejecutoras.faena.Etiqueta;
+import especie.Especie;
 import tropa.Animal;
 import tropa.Categoria;
 import tropa.Tropa;
 import servicios.AnimalDAO;
 import servicios.CategoriaDAO;
+import servicios.EspecieDAO;
 import servicios.TropaDAO;
 
 @ApplicationPath("/resources")
@@ -30,7 +34,9 @@ public class AnimalRest {
 		Categoria categoria = categoriaDAO.obtenerCategoria(animalBean.getIdCategoria());
 		
 		TropaDAO tropaDAO = new TropaDAO();
-		Tropa tropa = tropaDAO.obtenerTropa(animalBean.getIdTropa());
+		Tropa tropa = tropaDAO.obtenerTropaPorNroTropa(animalBean.getIdTropa());
+		
+		System.out.println(tropa);
 		
 		Animal animal = new Animal();
 		
@@ -42,17 +48,29 @@ public class AnimalRest {
 		 * POR AHOR NO TOMAMOS EL QUE PONE EL USUARIO
 		 * */
 		animal.setTropa(tropa);
-		animal.setGarron(tropaDAO.obtenerSiguienteNumeroDeGarron());
+		animal.setGarron(animalBean.getGarron());
 		animal.setCategoria(categoria);
 		animal.setPeso(animalBean.getPeso());
 		animal.setCabezaFaenadaEntera(animalBean.isCabezaFaenadaEntera());
 		
 		AnimalDAO animalDAO = new AnimalDAO();
 		animalDAO.salvarAnimal(animal);
-		Etiqueta etiqueta = new Etiqueta();
-		etiqueta.imprimirEtiquetas(tropa, animal);
+		//Etiqueta etiqueta = new Etiqueta();
+		//etiqueta.imprimirEtiquetas(tropa, animal);
 		
 		animalBean.setIdAnimal(animal.getIdAnimal());
 		return animalBean;
+	}
+	
+	@GET
+	@Path("/obtener_siguiente_garron")
+	@Produces(MediaType.APPLICATION_JSON)
+	public AnimalBean getGarron(){
+		System.out.println("El siguiente numero de garron es:  ");
+		AnimalBean animal = new AnimalBean();
+		TropaDAO tropaDAO = new TropaDAO();
+		int siguienteGarron = tropaDAO.obtenerSiguienteNumeroDeGarron();
+		animal.setGarron(siguienteGarron);
+		return animal;
 	}
 }
